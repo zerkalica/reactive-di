c0ab2925cfd17dd7d72a43d8a12dfb3f49ff3e27
Stefan Zerkalica <zerkalica@gmail.com>
2015-12-17 18:06:10 +0300

    restructired dirs

--- src/interfaces.js
+++ src/interfaces.js
@@ -4,7 +4,6 @@ export type DepId = string;
 export type NotifyFn = () => void;
 export type NotifyDepFn = (id: DepId) => void;
 export type NotifyDepsFn = (ids: Array<DepId>) => void;
-export type IPath = Array<string>;
 export type Setter<T: Object> = (model: T) => void;
 export type IdsMap = {[id: DepId]: Array<DepId>};
 export type Dependency<T> = Class<T>;
