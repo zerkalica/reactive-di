8039d6416d3a01d98d3f939b5339ad97570dffe5
Stefan Zerkalica <zerkalica@gmail.com>
2015-12-18 16:11:00 +0300

    ids map tests

--- src/meta/__tests__/updateIdsMapTest.js
+++ src/meta/__tests__/updateIdsMapTest.js
@@ -25,10 +25,10 @@ function updateMap(idToStateIdsMap: IdsMap, ...deps: Array<DepMeta>): IdsMap {
 }
 
 describe('updateIdsMapTest', () => {
-    it('should resolve deps', () => {
-        const a = createMeta('a');
-        const b = createMeta('b');
-        const c = createMeta('c');
+    it('should resolve deps B->a, A->b, C->c, state: {a: {b, c}}', () => {
+        const a = createMeta('a')
+        const b = createMeta('b')
+        const c = createMeta('c')
 
         const idToStateIdsMap: IdsMap = {
             a: ['a', 'b', 'c'],
@@ -46,6 +46,128 @@ describe('updateIdsMapTest', () => {
             a: ['A', 'B', 'C'],
             b: ['A', 'B'],
             c: ['B', 'C']
+        }
+        assert.deepEqual(stateIdToIdsMap, result)
+    })
+
+    it('should resolve deps B->A&b, A->a, state: {a, b}', () => {
+        const a = createMeta('a')
+        const b = createMeta('b')
+        const A = createMeta('A', a)
+        const B = createMeta('B', A, b)
+        const idToStateIdsMap: IdsMap = {
+            a: ['a'],
+            b: ['b']
+        };
+
+        const stateIdToIdsMap = updateMap(idToStateIdsMap, B)
+        const result = {
+            a: ['A', 'B'],
+            b: ['B']
+        }
+        assert.deepEqual(stateIdToIdsMap, result)
+    })
+
+    it('should resolve deps B->A&a, A->b, state: {a: {b}}', () => {
+        const a = createMeta('a')
+        const b = createMeta('b')
+        const A = createMeta('A', b)
+        const B = createMeta('B', A, a)
+        const idToStateIdsMap: IdsMap = {
+            a: ['a', 'b'],
+            b: ['b']
+        };
+
+        const stateIdToIdsMap = updateMap(idToStateIdsMap, B)
+        const result = {
+            a: ['B'],
+            b: ['A', 'B']
+        }
+        assert.deepEqual(stateIdToIdsMap, result)
+    })
+
+    it('should resolve deps B->A, C->B, A->a, state: {a}', () => {
+        const a = createMeta('a')
+        const idToStateIdsMap: IdsMap = {
+            a: ['a']
+        };
+        const A = createMeta('A', a)
+        const B = createMeta('B', A)
+        const C = createMeta('C', B)
+
+        const stateIdToIdsMap = updateMap(idToStateIdsMap, C)
+        const result = {
+            a: ['A', 'B', 'C']
+        }
+        assert.deepEqual(stateIdToIdsMap, result)
+    })
+
+    it('should resolve deps B->A, C->A, A->a, state: {a}', () => {
+        const a = createMeta('a')
+        const idToStateIdsMap: IdsMap = {
+            a: ['a']
+        };
+        const A = createMeta('A', a)
+        const B = createMeta('B', A)
+        const C = createMeta('C', A)
+
+        const stateIdToIdsMap = updateMap(idToStateIdsMap, B, C)
+        const result = {
+            a: ['A', 'B', 'C']
+        }
+        assert.deepEqual(stateIdToIdsMap, result)
+    })
+
+    it('should resolve deps A->C&B, B->C&b, C->a, D->B, state: {a, b}', () => {
+        const a = createMeta('a')
+        const b = createMeta('b')
+        const C = createMeta('C', a)
+        const B = createMeta('B', C, b)
+        const D = createMeta('D', B)
+        const A = createMeta('A', C, B)
+        const idToStateIdsMap: IdsMap = {
+            a: ['a'],
+            b: ['b']
+        };
+
+        const stateIdToIdsMap = updateMap(idToStateIdsMap, A, D)
+        const result = {
+            a: ['C', 'B', 'A', 'D'],
+            b: ['B', 'A', 'D']
+        }
+        assert.deepEqual(stateIdToIdsMap, result)
+    })
+
+    it('should resolve deps A->C&B, B->C&b, C->a, state: {a, b}', () => {
+        const a = createMeta('a')
+        const b = createMeta('b')
+        const C = createMeta('C', a)
+        const B = createMeta('B', C, b)
+        const A = createMeta('A', C, B)
+        const idToStateIdsMap: IdsMap = {
+            a: ['a'],
+            b: ['b']
+        };
+
+        const stateIdToIdsMap = updateMap(idToStateIdsMap, A)
+        const result = {
+            a: ['C', 'B', 'A'],
+            b: ['B', 'A']
+        }
+        assert.deepEqual(stateIdToIdsMap, result)
+    })
+
+    it('should resolve deps B->A&a, A->a, state: {a}', () => {
+        const a = createMeta('a')
+        const A = createMeta('A', a)
+        const B = createMeta('B', A, a)
+        const idToStateIdsMap: IdsMap = {
+            a: ['a']
+        };
+
+        const stateIdToIdsMap = updateMap(idToStateIdsMap, B)
+        const result = {
+            a: ['A', 'B']
         }
         assert.deepEqual(stateIdToIdsMap, result)
     })
