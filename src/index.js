09cac048eb310a885cdd8af1a1e4704ad1950e61
Stefan Zerkalica <zerkalica@gmail.com>
2016-04-08 15:53:12 +0300

    exposed SimpleMap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import BaseProvider from 'reactive-di/core/BaseProvider'
 import ReactiveDi from 'reactive-di/core/ReactiveDi'
 import defaultPlugins from 'reactive-di/plugins/defaultPlugins'
+import SimpleMap from 'reactive-di/utils/SimpleMap'
 
 export {
+    SimpleMap,
     BaseProvider,
     ReactiveDi,
     defaultPlugins
