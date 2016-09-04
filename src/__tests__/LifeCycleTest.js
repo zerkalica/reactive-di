71b24aebe42de27a0942be8d75e73013f914c241
Stefan Zerkalica <zerkalica@gmail.com>
2016-09-04 18:03:15 +0300

    tests fixed

--- src/__tests__/LifeCycleTest.js
+++ src/__tests__/LifeCycleTest.js
@@ -3,9 +3,9 @@
 
 import {spy, stub, match} from 'sinon'
 import assert from 'power-assert'
-import {Component as ReactComponent, createElement as h} from 'react'
-import ReactDOM from 'react-dom/server'
-import TestUtils from 'react-addons-test-utils'
+import {Component as RC, createElement} from 'react'
+import {findDOMNode as toHtml} from 'react-dom'
+import {renderIntoDocument} from 'react-addons-test-utils'
 
 import {
     theme,
@@ -19,13 +19,12 @@ import type {StyleSheet} from 'reactive-di/interfaces/component'
 
 import {Component} from 'fake-react'
 import {Di, Updater} from 'reactive-di/index'
-import BaseModel from 'reactive-di/utils/BaseModel'
 
 import createReactWidgetFactory from 'reactive-di/adapters/createReactWidgetFactory'
 import createHandlers from 'reactive-di/createHandlers'
 
-function findDOMElement(): HTMLElement {
-    throw new Error('get dom element not simport createHandlers unsupported on server')
+function render(raw) {
+    return renderIntoDocument(createElement(raw))
 }
 
 describe('LifeCycleTest', () => {
@@ -42,6 +41,7 @@ describe('LifeCycleTest', () => {
         const fakeFetch = spy(() => promise)
         const onMount = spy()
         const onUnmount = spy()
+        const onUpdate = spy()
 
         class Fetcher {
             fetch: (args: Object) => Promise<*> = fakeFetch
@@ -69,14 +69,16 @@ describe('LifeCycleTest', () => {
                 this._updater = updater
             }
 
-            onUpdate(): void {}
+            onUpdate(oldModelA: ModelA, newModelA: ModelA): void {
+                onUpdate(oldModelA, newModelA)
+            }
 
             onMount(model: ModelA) {
                 onMount(model)
                 const promise: Promise<ModelARec> = this._promise = this._fetcher.fetch({
                     url: '/model-a'
                 })
-                this._updater.setSingle(promise, ModelA)
+                this._updater.setSingle(() => promise, ModelA)
             }
 
             onUnmount(model: ModelARec) {
@@ -94,39 +96,53 @@ describe('LifeCycleTest', () => {
         @deps({m: ModelA})
         @component()
         class ComponentA extends Component<Props, State> {
-            props: Props;
-            state: State;
-            $: HTMLElement;
+            props: Props
+            state: State
+            $: HTMLElement
             render() {
                 const {m} = this.state
-                return h('div', null, 'testA-' + this.state.m.val)
+                return createElement('div', null, 'testA-' + this.state.m.val)
             }
         }
 
         @deps({m: ModelA})
         @component()
         class ComponentB extends Component<Props, State> {
-            props: Props;
-            state: State;
-            $: HTMLElement;
+            props: Props
+            state: State
+            $: HTMLElement
             render() {
                 const {m} = this.state
-                return h('div', null, 'testB-' + this.state.m.val)
+                return createElement('div', null, 'testB-' + this.state.m.val)
             }
         }
 
-        const handlers = createHandlers(createReactWidgetFactory(ReactComponent, findDOMElement))
+        const handlers = createHandlers(createReactWidgetFactory(RC, toHtml))
         const di = new Di(handlers)
+
         const ComponentAEl = di.val(ComponentA).get()
         const ComponentBEl = di.val(ComponentB).get()
-        const componentA = TestUtils.renderIntoDocument(h(ComponentAEl))
-        assert(onMount.calledOnce)
-        const componentB = TestUtils.renderIntoDocument(h(ComponentBEl))
-        assert(onMount.calledOnce)
-        assert(onUnmount.notCalled)
-        componentA.componentWillUnmount()
-        assert(onUnmount.notCalled)
-        componentB.componentWillUnmount()
-        assert(onUnmount.calledOnce)
+        const componentA = render(ComponentAEl)
+        const componentB = render(ComponentBEl)
+
+        assert(toHtml(componentA).textContent === 'testA-1')
+        assert(toHtml(componentB).textContent === 'testB-1')
+        assert(onUpdate.notCalled)
+
+        return promise.then(() => {
+            const componentA2 = render(ComponentAEl)
+            const componentB2 = render(ComponentBEl)
+            assert(toHtml(componentA2).textContent === 'testA-2')
+            assert(toHtml(componentB2).textContent === 'testB-2')
+            assert(onUpdate.calledOnce)
+
+            componentA.componentWillUnmount()
+            componentA2.componentWillUnmount()
+            componentB2.componentWillUnmount()
+            assert(onUnmount.notCalled)
+            componentB.componentWillUnmount()
+            assert(onMount.calledOnce)
+            assert(onUnmount.calledOnce)
+        })
     })
 })
