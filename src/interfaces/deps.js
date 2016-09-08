073e6f82cf98072a1ceec46b06f6414c85cf2092
Stefan Zerkalica <zerkalica@gmail.com>
2016-09-08 10:43:16 +0300

    scopes fix

--- src/interfaces/deps.js
+++ src/interfaces/deps.js
@@ -5,8 +5,8 @@ export type DepFn<V> = (...a: any) => V
 export type DepDict = {[k: string]: Key}
 export type ArgDep = Key | DepDict
 
-export type DepAlias = [Key, Function]
-export type RegisterDepItem = DepAlias | Key
+export type DepAlias = [Function, Function]
+export type RegisterDepItem = DepAlias | Function
 
 export type ResultOf<F> = _ResultOf<*, F>
 type _ResultOf<V, F: (...x: any[]) => V> = V
