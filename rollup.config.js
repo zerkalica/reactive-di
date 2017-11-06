7628d2f87f33dd7fb5b093fcaac32219dc64b1e8
Stefan Zerkalica <zerkalica@gmail.com>
2017-11-06 14:55:39 +0300

    updated deps

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,19 @@ import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 import {minify} from 'uglify-es'
-import babelrc from 'babelrc-rollup'
 
 import fs from 'fs'
+import path from 'path'
 
-const pkg = JSON.parse(fs.readFileSync('./package.json'))
+const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')))
+const babelrc = JSON.parse(fs.readFileSync(path.join(__dirname, '.babelrc')))
+
+const magic = 'commonjs'
+babelrc.babelrc = false
+babelrc.plugins = babelrc.plugins.map(
+    plugin => (Array.isArray(plugin) ? (plugin[0] || ''): plugin).indexOf(magic) >= 0 ? null : plugin
+).filter(Boolean)
+
 const uglifyOpts = {
     warnings: true,
     compress: {
@@ -26,7 +34,7 @@ const commonConf = {
     sourcemap: true,
     plugins: [
         resolve(),
-        babel(babelrc())
+        babel(babelrc)
     ].concat(process.env.UGLIFY === '1' ? [uglify(uglifyOpts, minify)] : []),
     output: [
         {file: pkg.module, format: 'es'},
--- src/createCreateElement.js
+++ src/createCreateElement.js
@@ -8,21 +8,23 @@ export default function createCreateElement<IElement, State, CreateElement: Func
     createElement: CreateElement
 ): CreateElement {
     function lomCreateElement() {
-        let el = arguments[0]
-        let attrs: ?Object = arguments[1]
+        const args = arguments
+        let attrs = args[1]
+        let el = args[0]
         let newEl
         const isAtomic = typeof el === 'function' && el.constructor.render === undefined
-        const id = attrs ? attrs.id : undefined
+        const id: string | void = attrs ? attrs._id || attrs.id : undefined
+        const parentContext: Injector | void = Injector.parentContext
         if (isAtomic) {
-            if (!attrs) {
-                attrs = {__lom_ctx: Injector.parentContext}
-            } else {
-                attrs.__lom_ctx = Injector.parentContext
-            }
-            if (Injector.parentContext !== undefined) {
-                newEl = Injector.parentContext.alias(el, id)
+            if (parentContext !== undefined) {
+                newEl = parentContext.alias(el, id)
                 if (newEl === null) return null
                 if (newEl !== undefined) el = newEl
+                if (!attrs) {
+                    attrs = {__lom_ctx: parentContext}
+                } else {
+                    attrs.__lom_ctx = parentContext
+                }
             }
 
             if (el.__lom === undefined) {
@@ -30,43 +32,42 @@ export default function createCreateElement<IElement, State, CreateElement: Func
             }
             newEl = el.__lom
         } else {
-            if (Injector.parentContext !== undefined && id) {
-                newEl = Injector.parentContext.alias(el, id)
+            if (parentContext !== undefined && id) {
+                newEl = parentContext.alias(el, id)
                 if (newEl === null) return null
                 if (newEl !== undefined) el = newEl
             }
             newEl = el
         }
-
-        switch(arguments.length) {
+        switch(args.length) {
             case 2:
                 return createElement(newEl, attrs)
             case 3:
-                return createElement(newEl, attrs, arguments[2])
+                return createElement(newEl, attrs, args[2])
             case 4:
-                return createElement(newEl, attrs, arguments[2], arguments[3])
+                return createElement(newEl, attrs, args[2], args[3])
             case 5:
-                return createElement(newEl, attrs, arguments[2], arguments[3], arguments[4])
+                return createElement(newEl, attrs, args[2], args[3], args[4])
             case 6:
-                return createElement(newEl, attrs, arguments[2], arguments[3], arguments[4], arguments[5])
+                return createElement(newEl, attrs, args[2], args[3], args[4], args[5])
             case 7:
-                return createElement(newEl, attrs, arguments[2], arguments[3],
-                    arguments[4], arguments[5], arguments[6])
+                return createElement(newEl, attrs, args[2], args[3],
+                    args[4], args[5], args[6])
             case 8:
-                return createElement(newEl, attrs, arguments[2], arguments[3],
-                    arguments[4], arguments[5], arguments[6], arguments[7])
+                return createElement(newEl, attrs, args[2], args[3],
+                    args[4], args[5], args[6], args[7])
             case 9:
-                return createElement(newEl, attrs, arguments[2], arguments[3],
-                    arguments[4], arguments[5], arguments[6], arguments[7], arguments[8])
+                return createElement(newEl, attrs, args[2], args[3],
+                    args[4], args[5], args[6], args[7], args[8])
             default:
                 if (isAtomic === false) {
-                    return createElement.apply(null, arguments)
+                    return createElement.apply(null, args)
                 }
-                const args = [newEl, attrs]
-                for (let i = 2, l = arguments.length; i < l; i++) {
-                    args.push(arguments[i])
+                const newArgs = [newEl, attrs]
+                for (let i = 2, l = args.length; i < l; i++) {
+                    newArgs.push(args[i])
                 }
-                return createElement.apply(null, args)
+                return createElement.apply(null, newArgs)
         }
     }
 
